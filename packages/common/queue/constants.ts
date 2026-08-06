28a81f7da43daec7ae4ef4e7e15cc29ad4cf445b
lyricalsoul <lyricalsoul@proton.me>
2026-08-05 20:33:44 -0400

    fix: add retention policy to bullmq

--- packages/common/queue/constants.ts
+++ packages/common/queue/constants.ts
@@ -7,3 +7,8 @@ export const RESUME_QUEUE_NAME = name('resume')
 export const QUICKVIEW_QUEUE_NAME = name('quickviews')
 export const PAGE_QUEUE_NAME = name('pages')
 export const UPLOAD_QUEUE_NAME = name('uploads')
+
+export const DEFAULT_JOB_RETENTION = {
+  removeOnComplete: { age: 60 * 60 * 24, count: 10_000 },
+  removeOnFail: { age: 60 * 60 * 24 * 7, count: 5_000 },
+} as const
--- packages/common/queue/index.ts
+++ packages/common/queue/index.ts
@@ -1,5 +1,5 @@
 import { createNodeRedisClient, Queue, QueueEvents } from 'bullmq'
-import { COMMAND_QUEUE_NAME, RESPONSE_QUEUE_NAME, RESUME_QUEUE_NAME, QUICKVIEW_QUEUE_NAME, PAGE_QUEUE_NAME, UPLOAD_QUEUE_NAME } from './constants'
+import { COMMAND_QUEUE_NAME, RESPONSE_QUEUE_NAME, RESUME_QUEUE_NAME, QUICKVIEW_QUEUE_NAME, PAGE_QUEUE_NAME, UPLOAD_QUEUE_NAME, DEFAULT_JOB_RETENTION } from './constants'
 import { rawClient, ensureRedisConnected } from '../redis'
 
 export { rawClient }
@@ -7,11 +7,11 @@ await ensureRedisConnected()
 
 export const connection = createNodeRedisClient(rawClient)
 
-export const commandQueue = new Queue(COMMAND_QUEUE_NAME, { connection })
-export const responseQueue = new Queue(RESPONSE_QUEUE_NAME, { connection })
-export const resumeQueue = new Queue(RESUME_QUEUE_NAME, { connection })
-export const quickViewQueue = new Queue(QUICKVIEW_QUEUE_NAME, { connection })
-export const pageQueue = new Queue(PAGE_QUEUE_NAME, { connection })
-export const uploadQueue = new Queue(UPLOAD_QUEUE_NAME, { connection })
+export const commandQueue = new Queue(COMMAND_QUEUE_NAME, { connection, defaultJobOptions: DEFAULT_JOB_RETENTION })
+export const responseQueue = new Queue(RESPONSE_QUEUE_NAME, { connection, defaultJobOptions: DEFAULT_JOB_RETENTION })
+export const resumeQueue = new Queue(RESUME_QUEUE_NAME, { connection, defaultJobOptions: DEFAULT_JOB_RETENTION })
+export const quickViewQueue = new Queue(QUICKVIEW_QUEUE_NAME, { connection, defaultJobOptions: DEFAULT_JOB_RETENTION })
+export const pageQueue = new Queue(PAGE_QUEUE_NAME, { connection, defaultJobOptions: DEFAULT_JOB_RETENTION })
+export const uploadQueue = new Queue(UPLOAD_QUEUE_NAME, { connection, defaultJobOptions: DEFAULT_JOB_RETENTION })
 
 export const responseQueueEvents = new QueueEvents(RESPONSE_QUEUE_NAME, { connection })
